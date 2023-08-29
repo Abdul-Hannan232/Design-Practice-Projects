0e3e9e8020e24890bd0cef1a7391cfe36adb784f
Abdul Hannan <abdulhani232@gmail.com>
2023-08-30 01:56:27 +0500

    Project 2 using LocalStorage Data
    Receive data from local storage and use it to update UI when page gets refresh

--- project02/script.js
+++ project02/script.js
@@ -8,6 +8,8 @@ let seats = document.querySelectorAll(".row .seat:not(.occupied)");
 let count = document.getElementById("count");
 let price = document.getElementById("price");
 
+populateUI();
+
 function updateCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
@@ -16,7 +18,7 @@ function updateCount() {
   count.innerText = selectedSeatsCount;
   price.innerText = selectedSeatsCount * moviePrice;
 
-  localStorage.setItem("Selected Movies", JSON.stringify(seatsIndex));
+  localStorage.setItem("Selected Seats", JSON.stringify(seatsIndex));
 }
 
 // Funtion for saving movie data to local storage
@@ -24,6 +26,23 @@ function saveMovieData(movieIndex, moviePrice) {
   console.log(movieIndex, moviePrice);
   localStorage.setItem("Selected Movies Index", movieIndex);
   localStorage.setItem("Selected Movies Price", moviePrice);
+}
+
+// Use data stored in Local Storage
+function populateUI() {
+  const selectedSeatsIndex = JSON.parse(localStorage.getItem("Selected Seats"));
+  if (selectedSeatsIndex !== null && selectedSeatsIndex.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeatsIndex.indexOf(index) > -1) {
+        seat.classList.toggle("selected");
+      }
+    });
+  }
+
+  const selectedMoviesIndex = localStorage.getItem("Selected Movies Index");
+  if (selectedMoviesIndex > -1) {
+    movie.selectedIndex = selectedMoviesIndex;
+  }
 }
 
 // Event Listeners
@@ -44,3 +63,6 @@ movie.addEventListener("change", (e) => {
   saveMovieData(e.target.selectedIndex, e.target.value);
   updateCount();
 });
+
+// calling UpdateCount function in the end to update UI ( Initial movie count and total price ) if getting data from LocalStorage
+updateCount();
