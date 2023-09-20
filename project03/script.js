9d4edd5073905fda7f5969ac006d60f86929b365
Abdul Hannan <abdulhani232@gmail.com>
2023-09-20 23:35:31 +0500

    Project 3 Event Listeners
    Added Event Listeners before creating functions

--- project03/script.js
+++ project03/script.js
@@ -1,5 +1,18 @@
+// Get DOM elements for JS code
 const video = document.getElementById("video");
 const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progressBar = document.getElementById("progress");
 const timer = document.getElementById("timer");
+
+// Event Listeners for video
+video.addEventListener("click", toggleStatus);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("stop", updatePlayIcon);
+
+// Event Listener for Icons Play / Stop
+play.addEventListener("click", updatePlayIcon);
+stop.addEventListener("clicl", stopVideo);
+
+// Event listener for Timer
+progressBar.addEventListener("change", updateProgress);
