eb0703dd64af208ad4d95855d0a5f4ff2121a4ae
Abdul Hannan <abdulhani232@gmail.com>
2023-09-29 23:51:04 +0500

    Project 4 - Added class, added styles, get DOM Elemnts

---
+++ project05/script.js
@@ -0,0 +1,7 @@
+// Get DOM Elements
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const filterBtn = document.getElementById("filter");
+const sortBtn = document.getElementById("sort");
+const sumBtn = document.getElementById("sum");
